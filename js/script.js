f15f5b377c5ece1168e3d60f875f136b73c7aa17
Andrey <andreymekshun@gmail.com>
2023-03-01 18:41:23 +0200

    first commit

---
+++ js/script.js
@@ -0,0 +1,63 @@
+'use strict';
+
+// alert('Hello'); 
+
+// eslint-disable-next-line no-unused-vars
+// const result = confirm('Are you here?');
+// console.log(result);
+
+// const answer = +prompt('DA?', '20');
+// console.log(answer + 5);
+
+// const answers = [];
+
+// answers[0] = prompt('DA?', '');
+// answers[1] = prompt('NET?', '');
+// answers[2] = prompt('KTO?', '');
+
+// console.log(typeof(answers));
+
+
+
+
+// const category = 'toys';
+
+
+// // Старый формат
+// // console.log('https://soumeurl.com/' + category + '/' + '4');
+
+
+// // Новый формат
+// console.log(`https://soumeurl.com/${category}/4`);
+
+
+// const user = 'Andrey';
+
+// alert(`Hello, ${user}`);
+// console.log('arr' + '- object');
+// console.log(4 + +'5');
+
+let incr = 10,
+    decr = 10;
+
+// incr++;
+// decr--;
+
+console.log(++incr);
+console.log(--decr);
+
+console.log(5%2); 
+
+console.log(2*4 === '8');
+// % - остаток от деления 
+// = - присваивание 
+// == - сравнение 
+// === сравнение по типу данных
+// && - И
+// ||- или 
+// !- отрицание (меняет значение на обратное)
+
+const biba = true,
+    boba = false; 
+
+console.log(biba || boba);
