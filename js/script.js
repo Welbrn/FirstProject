56805553aa53b58e8f8c4f22d12612b79283860a
Andrey <andreymekshun@gmail.com>
2023-03-03 15:37:34 +0200

    Home work  2

--- js/script.js
+++ js/script.js
@@ -66,21 +66,21 @@
 const  numberOfFilm = +prompt ('Сколько фильмов посмотрели', '');
 
 const personaLMovieDB = {
-    count: numberOfFilm,
-    movies: {
+	count: numberOfFilm,
+	movies: {
     
-    },
-    actors: {
+	},
+	actors: {
 
-    },
-    genres: [],
-    private: false
+	},
+	genres: [],
+	private: false
 };
 
-    const a = prompt('один из последних просмотренных фильмов', ''),
-          b = prompt('на сколько оцените его?', ''),
-          c = prompt('один из последних просмотренных фильмов', ''),
-          d = prompt('на сколько оцените его?', '');
+const a = prompt('один из последних просмотренных фильмов', ''),
+	b = prompt('на сколько оцените его?', ''),
+	c = prompt('один из последних просмотренных фильмов', ''),
+	d = prompt('на сколько оцените его?', '');
 
 
 personaLMovieDB.movies[a] = b;
@@ -207,7 +207,7 @@ console.log(personaLMovieDB);
 // for (let  i =  1; i < 10; i++) {
 //     if (i === 6) {
 //         // break;
-           // continue;
+// continue;
 //     }
 
 //     console.log(i);
@@ -253,3 +253,173 @@ console.log(personaLMovieDB);
 // }
 ///////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
+// const arr = [3, 5, 8, 16, 20, 23, 50];
+//     const result = [];
+    
+//     for (let i = 0; i < arr.length; i++) {
+//    result[i] = arr[i];
+// }
+
+
+
+
+// const data = [5, 10, 'Shopping', 20, 'Homework'];
+
+// for (let i = 0; i < data.length; i++) {
+//     if (typeof(data[i]) === 'number') {
+//         data[i] = data[i] * 2;
+//     } else if (typeof(data[i]) === 'string') {
+//         data[i] = `${data[i]} - done`;
+//     }
+// }
+
+// console.log(data);
+// return data;
+
+
+
+
+
+
+
+//     const data = [5, 10, 'Shopping', 20, 'Homework'];
+//     const result = [];
+
+//     for (let i = 0, j = data.length - 1; i < data.length; i++, j--) {
+//   result[i] = data[j];
+// }
+
+// console.log(result);
+    
+//     return result;
+
+
+
+
+
+// for (let i = 0; i < 3; i++) {
+//     console.log(i);
+//     for (let j = 0; j < 3; j++) {
+//         console.log(j);
+//     }
+// }
+
+
+// let result = '';
+// const dlina = 7;
+
+// for (let i = 1; i < dlina; i++) {
+
+// 	for (let j = 0; j < i; j++) {
+// 		result += '*';
+// 	}
+// 	result += '\n';
+// }
+
+
+// console.log(result);
+
+///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+////////////////////////////////////////////////// HOME WORK////////////////////////////////////////////////////////////
+
+
+// for (let i  = 5; i <= 10; i++) {
+// 	console.log(i);
+// }
+///////////////////////////////////////////
+
+// for (let i = 20; i >= 10; i--) {
+// 	if (i === 13) {
+// 		break;
+// 	}
+// 	console.log(i);
+// }
+/////////////////////////////////////////////
+
+// for (let i = 2; i <= 10; i++) {
+// 	if (i % 2 === 0){
+// 		console.log(i);
+// 	}
+// }
+////////////////////////////////////////////
+
+// for (let i = 2; i <= 16; i++) {
+// 	if (i % 2 === 0) {
+// 		continue;
+// 	} else {
+// 		console.log(i);
+// 	}
+// }
+
+
+
+// let i = 2;
+
+//     while (i <= 16) {
+//         if (i % 2 === 0) {
+//             i++;
+//             continue;
+//         } else {
+//             console.log(i);
+//         }
+//         i++;
+//     }
+///////////////////////////////////////////////
+
+// const arrayOfNumbers = [];
+
+// for (let i = 5; i <= 10; i++) {
+//     arrayOfNumbers[i - 5] = i;
+// }
+//     console.log(arrayOfNumbers);
+//     return arrayOfNumbers;
+/////////////////////////////////////////////////
+
+// const arr = [3, 5, 8, 16, 20, 23, 50];
+// const result = [];
+
+// for(let i =  0; i < arr.length; i++) {
+//     result[i] = arr[i];
+// }
+// console.log(result);
+////////////////////////////////////////////////
+
+// const data = [5, 10, 'Shopping', 20, 'Homework'];
+
+// for(let i = 0; i < data.length; i++) {
+//     if (typeof(data[i]) === 'number') {
+//     data[i] = data[i] * 2;
+//     } else {
+//         if (typeof(data[i]) === 'string') {
+//         data[i] = `${data[i]} - done`;
+//         }
+//     } 
+// } 
+// console.log(data);
+/////////////////////////////////////////////////
+
+// const data = [5, 10, 'Shopping', 20, 'Homework'];
+//     const result = [];
+//     for(let i = 0, j = data.length - 1; i < data.length; i++, j--) {
+//         result[i] = data[j];
+//     }
+//     console.log(result);
+/////////////////////////////////////////////////////////
+
+// const lines = 5;
+// let result = '';
+
+// for (let i = 0; i <= lines; i++) {
+//     for (let j = 0; j < lines - i; j++) {
+//         result += " ";
+//     }
+//     for (let j = 0; j < 2 * i + 1; j++) {
+//         result += "*";
+//     }
+//     result += "\n";
+// }
+
+// console.log(result)
+////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
